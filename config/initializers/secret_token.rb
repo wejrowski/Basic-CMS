5ad3a8ea0fecef985760fb90988a7a12302ef935
Brent Wejrowski <wejrowski@gmail.com>
2011-11-22 09:04:16 -0700

    Rails 3.1.3 + activeadmin gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BasicCms::Application.config.secret_token = 'c1c8ceb300c22b8d2d78f30904054eaef4759699074f9175628213fd8df6cb3bf584870963b948e5250d56b26085aee7e181bf207e8f1b42ca0fff908f4a367b'
